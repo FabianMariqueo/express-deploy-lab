968766907fd5c392af5a4678500a2da9651f71d6
Fabian <fabianmariqueo@gmail.com>
2022-02-17 16:13:43 -0300

    fix: gcp_network version and variable name

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,7 +17,7 @@ resource "local_file" "kubeconfig" {
 
 module "gcp-network" {
   source       = "terraform-google-modules/network/google"
-  version      = "~> 2.5"
+  version      = "4.1.0"
   project_id   = var.project_id
   network_name = "${var.network}-${var.env_name}"
   subnets = [
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
   credentials = file("/home/fabian/gcp-credentials/ninth-palace-335210-8ce673ee53fc.json")
-  project = var.project
+  project = var.project_id
   region = var.region
 }
